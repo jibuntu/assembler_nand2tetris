7eab39900c589e9824e3442234b88be88c4acc54
jibuntu <i.am.tomotatu@gmail.com>
2020-04-21 01:42:21 +0900

    SymbolTableモジュールを作成

---
+++ src/symbol_table/mod.rs
@@ -0,0 +1,60 @@
+// nand2tetris - page 125
+
+use std::collections::HashMap;
+
+/// symbolの名前とアドレスを管理するためのモジュール
+pub struct SymbolTable {
+    table: HashMap<String, usize>
+}
+
+impl SymbolTable {
+    pub fn new() -> SymbolTable {
+        SymbolTable {
+            table: HashMap::new()
+        }
+    }
+
+    pub fn add_entry(&mut self, symbol: &str, address: usize) {
+        self.table.insert(symbol.to_string(), address);
+    }
+
+    pub fn contains(&self, symbol: &str) -> bool {
+        self.table.contains_key(symbol)
+    }
+
+    pub fn get_address(&self, symbol: &str) -> Option<&usize> {
+        self.table.get(symbol)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::SymbolTable;
+
+    #[test]
+    fn test_symbol_table_new() {
+        let symbol_table = SymbolTable::new();
+    }
+
+    #[test]
+    fn test_symbol_table_add_entry() {
+        let mut symbol_table = SymbolTable::new();
+        symbol_table.add_entry("test", 10);
+        symbol_table.add_entry("aa", 0);
+    }
+
+    #[test]
+    fn test_symbol_table_contains() {
+        let mut symbol_table = SymbolTable::new();
+        symbol_table.add_entry("test", 10);
+        assert_eq!(symbol_table.contains("test"), true);
+        assert_eq!(symbol_table.contains("m"), false);
+    }
+
+    #[test]
+    fn test_symbol_table_get_address() {
+        let mut symbol_table = SymbolTable::new();
+        symbol_table.add_entry("test", 10);
+        assert_eq!(symbol_table.get_address("test"), Some(&10));
+    }
+}
